88d0ae20f6b5cee0546a8202568b4bc8d02b88cc
mercelisw <mercelisw@gmail.com>
2021-01-13 17:12:20 +0100

    added main function

--- parse_LSJ.py
+++ parse_LSJ.py
@@ -3,92 +3,6 @@ import xml.etree.ElementTree as ET
 
 # TEI.2 -> (teiHeader) text -> (front) body -> div0 -> (head) entryFree
 
-# for i, letter in enumerate(os.listdir('LSJ_data')):
-#
-#     title = 'LSJ_{}.csv'.format(i + 1)
-#
-#     with open('LSJ_output/' + title, "w+", encoding='UTF-8') as file:
-#
-#         my_tree = ET.parse("LSJ_data/" + letter)
-#
-#         my_root = my_tree.getroot()
-#
-#         if i == 0:
-#
-#             words = my_root[1][1][0][1:]  # TEI.2 -> (teiHeader) text -> (front) body -> div0 -> (head) entryFree
-#
-#         else:
-#
-#             words = my_root[1][0][0][1:]  # TEI.2 -> (teiHeader) text -> body -> div0 -> (head) entryFree
-#
-#         for j, word in enumerate(words):
-#             if word.tag != 'entryFree':
-#                 continue
-#             else:
-#                 if j % 1000 == 0:
-#                     print("book {}/{}, word {}/{}".format(i + 1, len((os.listdir("LSJ_data"))), j, len(words)))
-#
-#                 previous_sense_levels = ['A', 'I', '1', 'a']
-#
-#                 id = word.attrib['id']
-#                 key = word.attrib['key']
-#                 senses = word.findall("sense")
-#                 for sense in senses:
-#                     default = ['A', 'I', '1', 'a']
-#
-#                     name = sense.attrib['n']
-#                     level = sense.attrib['level']
-#
-#                     sense_levels = previous_sense_levels[:int(level) - 1] + [name] + default[int(level):]
-#
-#                     previous_sense_levels = sense_levels
-#                     translation = ""
-#                     bib_key = ""
-#                     translation_counter = 0
-#                     for element in sense[:]:  # skips the 'etymological' info
-#
-#                         if element.tag == 'tr' and translation_counter == 0:
-#
-#                             translation = element.text
-#                             if bib_key != "":
-#                                 file.write(
-#                                     id[1:] + '\t' + key + '\t' + "\t".join(sense_levels) + "\t" + translation + "\t" + bib_key + "\n")
-#                             translation_counter += 1
-#
-#                         if element.tag == 'bibl':  # bibliography without citations
-#
-#                             bib_key = ""
-#
-#                             if element.text is not None:
-#                                 if 'n' in element.attrib:
-#                                     bib_key += element.attrib['n']
-#                                 else:
-#                                     for child in element[:]:
-#                                         if child.text is not None:
-#                                             bib_key += child.text
-#
-#                             if translation_counter > 0:
-#                                 file.write(
-#                                     id[1:] + '\t' + key + '\t' + "\t".join(sense_levels) + "\t" + translation + "\t" + bib_key + "\n")
-#
-#                         if element.tag == 'cit':  # bibliography for citations
-#                             book = element.find('bibl')
-#                             bib_key = ""
-#
-#                             if book is not None:
-#
-#                                 if 'n' in book.attrib:
-#                                     bib_key += book.attrib['n']
-#                                 else:
-#                                     for child in book[:]:
-#                                         if child.text is not None:
-#                                             bib_key += child.text
-#
-#                                 if translation_counter > 0:
-#                                     file.write(
-#                                         id[1:] + '\t' + key + '\t' + "\t".join(sense_levels) + "\t" + translation + "\t" + bib_key + "\n")
-
-
 def create_abbrev_dict(file):
     with open(file, "r+") as input:
         lines = input.readlines()
@@ -98,6 +12,98 @@ def create_abbrev_dict(file):
             line = line.split('\t')
             authors[line[1]] = line[0]
         return authors
+
+def main():
+    authors = create_abbrev_dict("abbrev_authors.csv")
+    works = create_abbrev_dict("abbrev_works.csv")
+
+    for i, letter in enumerate(os.listdir('LSJ_data')):
+
+        title = 'LSJ_{}.csv'.format(i + 1)
+
+        with open('LSJ_output/' + title, "w+", encoding='UTF-8') as file:
+
+            my_tree = ET.parse("LSJ_data/" + letter)
+
+            my_root = my_tree.getroot()
+
+            if i == 0:
+
+                words = my_root[1][1][0][1:]  # TEI.2 -> (teiHeader) text -> (front) body -> div0 -> (head) entryFree
+
+            else:
+
+                words = my_root[1][0][0][1:]  # TEI.2 -> (teiHeader) text -> body -> div0 -> (head) entryFree
+
+            for j, word in enumerate(words):
+                if word.tag != 'entryFree':
+                    continue
+                else:
+                    if j % 1000 == 0:
+                        print("book {}/{}, word {}/{}".format(i + 1, len((os.listdir("LSJ_data"))), j, len(words)))
+
+                    previous_sense_levels = ['A', 'I', '1', 'a']
+
+                    id = word.attrib['id']
+                    key = word.attrib['key']
+                    senses = word.findall("sense")
+                    for sense in senses:
+                        default = ['A', 'I', '1', 'a']
+
+                        name = sense.attrib['n']
+                        level = sense.attrib['level']
+
+                        sense_levels = previous_sense_levels[:int(level) - 1] + [name] + default[int(level):]
+
+                        previous_sense_levels = sense_levels
+                        translation = ""
+                        bib_key = ""
+                        translation_counter = 0
+                        for element in sense[:]:  # skips the 'etymological' info
+
+                            if element.tag == 'tr' and translation_counter == 0:
+
+                                translation = element.text
+                                if bib_key != "":
+                                    file.write(
+                                        id[1:] + '\t' + key + '\t' + "\t".join(sense_levels) + "\t" + translation + "\t" + bib_key + "\n")
+                                translation_counter += 1
+
+                            if element.tag == 'bibl':  # bibliography without citations
+
+                                bib_key = ""
+
+                                if element.text is not None:
+                                    if 'n' in element.attrib:
+                                        bib_key += element.attrib['n']
+                                    else:
+                                        for child in element[:]:
+                                            if child.text is not None:
+                                                bib_key += child.text
+
+                                if translation_counter > 0:
+                                    file.write(
+                                        id[1:] + '\t' + key + '\t' + "\t".join(sense_levels) + "\t" + translation + "\t" + bib_key + "\n")
+
+                            if element.tag == 'cit':  # bibliography for citations
+                                book = element.find('bibl')
+                                bib_key = ""
+
+                                if book is not None:
+
+                                    if 'n' in book.attrib:
+                                        bib_key += book.attrib['n']
+                                    else:
+                                        for child in book[:]:
+                                            if child.text is not None:
+                                                bib_key += child.text
+
+                                    if translation_counter > 0:
+                                        file.write(
+                                            id[1:] + '\t' + key + '\t' + "\t".join(sense_levels) + "\t" + translation + "\t" + bib_key + "\n")
+
+
+
 
 
 authors = create_abbrev_dict("abbrev_authors.csv")
