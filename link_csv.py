e8d8afbca1e92189fa9cca9dbd12d1f53d8b1dd9
mercelisw <mercelisw@gmail.com>
2021-01-19 12:15:02 +0100

    fixed output

--- link_csv.py
+++ link_csv.py
@@ -6,7 +6,7 @@ pattern = r".perseus-\w+\d" #to remove .perseus-grc1 etc.
 def urn_to_ids(ref: str):  # Returns a dictionary containing the doc-id and the subdoc
     temp_ref = re.sub(pattern, '', ref)
     temp_ref = ''.join(digit for digit in temp_ref if not digit.isalpha())  #removes other non-numerics
-    temp_ref = temp_ref[3:-1]                                                #removes initial : and final character
+    temp_ref = temp_ref[3:]                                                #removes initial :
     temp_ref = temp_ref.replace(':', '.')                                   #removes remaining :
     cleaned_ref = temp_ref.replace('.', ':', 2)                      #split between author/work/subdoc is : otherwise .
 
@@ -40,9 +40,10 @@ for row in lsj.itertuples():
     if reference.startswith('urn'):
         bibliography = urn_to_ids(reference)
 
-        if bibliography['subdoc'] != "":
-            mask = ((xml['doc'].values == bibliography['doc']) & (xml['subdoc'].values == bibliography['subdoc']) \
-                   & (xml['lemma'].values == key))
+        if bibliography['subdoc'].isdigit() :
+            mask = ((xml['doc'].values == bibliography['doc']) &
+                    (xml['subdoc'].values == str(int(bibliography['subdoc']) - 1)) &
+                    (xml['lemma'].values == key))
             doc_subdoc = xml[mask]
             for doc_row in doc_subdoc.itertuples():
 
@@ -54,5 +55,5 @@ for row in lsj.itertuples():
 
     lsj.at[row.Index, 'word_id'] = result
 
-lsj.to_csv('tst/LSJ_2_wid.csv', sep='\t', encoding = 'UTF-8')
+lsj.to_csv('tst/LSJ_2_wid_only_direct.csv', sep='\t', encoding = 'UTF-8')
 
