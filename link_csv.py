aba2c3e01007bcf576054f416277d61464dd04a8
mercelisw <mercelisw@gmail.com>
2021-01-19 17:33:38 +0100

    first result: 1545 hits on ca 8000 words

--- link_csv.py
+++ link_csv.py
@@ -18,14 +18,14 @@ def urn_to_ids(ref: str):  # Returns a dictionary containing the doc-id and the 
     return {'doc': reference_fields[0] + '-' + reference_fields[1], 'subdoc': str(reference_fields[2])}
 
 
-xml = pd.read_csv('lemma_lookup.csv', sep='\t', encoding='UTF-8', names=['doc', 'subdoc', 'sentence', 'word', 'lemma'],
-                 dtype={'doc': str, 'subdoc': str, 'sentence': int, 'word': int, 'lemma': str})
+xml = pd.read_csv('lemma_lookup.csv', sep='\t', encoding='UTF-8', names=['doc', 'subdoc', 'sentence', 'line', 'word', 'lemma'],
+                 dtype={'doc': str, 'subdoc': str, 'sentence': int, 'line': str, 'word': int, 'lemma': str})
 
 lsj = pd.read_csv('tst/LSJ_2.csv', sep='\t', encoding='UTF-8', names=['id', 'key', 'sense_1', 'sense_2', 'sense_3',
                                                                       'sense_4', 'translation', 'ref'],
                   dtype={'id': int, 'key': str, 'sense_1': str, 'sense_2': str, 'sense_3' : int, 'sense_4': str,
                          'translation': str, 'ref': str})
-lemma = []
+nb_of_results = 0
 lsj['word_id'] = ""
 
 
@@ -40,20 +40,76 @@ for row in lsj.itertuples():
     if reference.startswith('urn'):
         bibliography = urn_to_ids(reference)
 
-        if bibliography['subdoc'].isdigit() :
+        if bibliography['subdoc'].isdigit() :                   # subdoc is only digits
+
+            zero_five_line_number = int(bibliography['subdoc']) // 5 * 5
+            if zero_five_line_number == 0:
+                zero_five_line_number = 1
             mask = ((xml['doc'].values == bibliography['doc']) &
-                    (xml['subdoc'].values == str(int(bibliography['subdoc']) - 1)) &
+                    ((xml['subdoc'].values == str(bibliography['subdoc'])) |
+                     (xml['subdoc'].values == str(int(bibliography['subdoc']) - 1)) |       #for off-by-ones in subdoc
+                     (xml['subdoc'].values == str(int(bibliography['subdoc']) + 1)) |
+                     (xml['subdoc'].values == str(zero_five_line_number))) &
                     (xml['lemma'].values == key))
             doc_subdoc = xml[mask]
             for doc_row in doc_subdoc.itertuples():
-
                 word_id = doc_row.word
-                result.append(word_id)
+                result.append(str(word_id))
+                nb_of_results += 1
+
+
+        elif bibliography['subdoc'].isalpha():                  # try line number
+            zero_five_line_number = int(bibliography['subdoc']) // 5 * 5
+            if zero_five_line_number == 0:
+                zero_five_line_number = 1
+            mask = ((xml['doc'].values == bibliography['doc']) &
+                    ((xml['line'].values == str(bibliography['subdoc'])) |
+                     (xml['line'].values == str(int(bibliography['subdoc']) - 1)) |            #for off-by-ones in line
+                     (xml['line'].values == str(int(bibliography['subdoc']) + 1)) |
+                     (xml['line'].values == str(zero_five_line_number))) &
+                    (xml['lemma'].values == key))
+            doc_subdoc = xml[mask]
+            for doc_row in doc_subdoc.itertuples():
+                word_id = doc_row.word
+                result.append(str(word_id))
+                nb_of_results += 1
+
+        if len(result) == 0 and len(bibliography['subdoc']) > 0:    #for subdocs like 1.2.3 or others
+            mask = ((xml['doc'].values == bibliography['doc']) &
+                    ((xml['subdoc'].values == str(bibliography['subdoc'])) |
+                     (xml['subdoc'].values == (bibliography['subdoc'][:-1]))) &     # e.g. Plato ... 23b
+                    (xml['lemma'].values == key))
+            doc_subdoc = xml[mask]
+            for doc_row in doc_subdoc.itertuples():
+                word_id = doc_row.word
+                result.append(str(word_id))
+                nb_of_results += 1
+
+        elif len(result) == 0:                                                       # if nothing found try whole document
+            mask = ((xml['doc'].values == bibliography['doc']) &
+                    (xml['lemma'].values == key))
+            doc_subdoc = xml[mask]
+            for doc_row in doc_subdoc.itertuples():
+                word_id = doc_row.word
+                result.append(str(word_id))
+                nb_of_results += 1
+
+        else:
+            mask = ((xml['doc'].values == bibliography['doc']))
+            doc_subdoc = xml[mask]
+            if doc_subdoc.size == 0:
+                result.append('Document not found')
+                doc_not_found += 1
 
     else:
         pass             #TODO handling of non-urn links!!
 
+
+    print(str(row.Index) + ': ' + ','.join(result))
     lsj.at[row.Index, 'word_id'] = result
 
-lsj.to_csv('tst/LSJ_2_wid_only_direct.csv', sep='\t', encoding = 'UTF-8')
+print("Hits: " + str(nb_of_results))
+print("Documents not found: " + str(doc_not_found))
+
+lsj.to_csv('tst/LSJ_2_wid.csv', sep='\t', encoding = 'UTF-8')
 
--- xml_to_txt.py
+++ xml_to_txt.py
@@ -5,8 +5,6 @@ missing_subdocs = []
 missing_lemmata = []
 
 with open('lemma_lookup.csv', 'w+', encoding='UTF-8') as output:
-
-
     for xml in os.listdir('parsed_xmls'):
         read_xml = ET.parse('parsed_xmls/' + xml)
         root = read_xml.getroot()
@@ -14,28 +12,57 @@ with open('lemma_lookup.csv', 'w+', encoding='UTF-8') as output:
         for sentence in root[:]:
 
             for word in sentence[:]:
+                if 'wid' in word.attrib:
+                    if int(word.attrib['wid']) % 10000 == 0:
+                        print(word.attrib['wid'])
 
-                if int(word.attrib['wid']) % 10000 == 0:
-                    print(word.attrib['wid'])
+                    if 'lemma' in word.attrib and 'subdoc' in sentence.attrib:
+                        if 'line' in word.attrib:
+                            output.write(
+                                sentence.attrib['document_id'] + '\t' + sentence.attrib['subdoc'] + '\t' + sentence.attrib[
+                                    'id'] + '\t' +
+                                word.attrib['line'] + '\t' + word.attrib['wid'] + '\t' + word.attrib['lemma'] + '\n')
+                        else:
+                            output.write(
+                                sentence.attrib['document_id'] + '\t' + sentence.attrib['subdoc'] + '\t' + sentence.attrib[
+                                    'id'] + '\t' +
+                                '' + '\t' + word.attrib['wid'] + '\t' + word.attrib['lemma'] + '\n')
 
-                if 'lemma' in word.attrib and 'subdoc' in sentence.attrib:
-                    output.write(sentence.attrib['document_id'] + '\t' + sentence.attrib['subdoc'] + '\t' + sentence.attrib['id'] + '\t' +
-                          word.attrib['wid'] + '\t' + word.attrib['lemma'] + '\n')
+                    elif 'lemma' in word.attrib:
+                        if 'line' in word.attrib:
+                            output.write(sentence.attrib['document_id'] + '\t' + '' + '\t' + sentence.attrib['id'] + '\t' +
+                                         word.attrib['line'] + '\t' + word.attrib['wid'] + '\t' + word.attrib[
+                                             'lemma'] + '\n')
+                            missing_subdocs.append(sentence.attrib['document_id'])
+                        else:
+                            output.write(sentence.attrib['document_id'] + '\t' + '' + '\t' + sentence.attrib['id'] + '\t'
+                                         + '' + '\t' + word.attrib['wid'] + '\t' + word.attrib['lemma'] + '\n')
+                            missing_subdocs.append(sentence.attrib['document_id'])
 
-                elif 'lemma' in word.attrib:
-                    output.write(sentence.attrib['document_id'] + '\t' + '' + '\t' + sentence.attrib['id'] + '\t' +
-                          word.attrib['wid'] + '\t' + word.attrib['lemma'] + '\n')
-                    missing_subdocs.append(sentence.attrib['document_id'])
+                    elif 'subdoc' in sentence.attrib:
+                        if 'line' in word.attrib:
+                            output.write(
+                                sentence.attrib['document_id'] + '\t' + sentence.attrib['subdoc'] + '\t' + sentence.attrib[
+                                    'id'] + '\t' + word.attrib['line'] + '\t' +
+                                word.attrib['wid'] + '\t' + '' + '\n')
+                            missing_lemmata.append(word.attrib['wid'])
 
-                elif 'subdoc' in sentence.attrib:
-                    output.write(sentence.attrib['document_id'] + '\t' + sentence.attrib['subdoc']+ '\t' + sentence.attrib['id'] + '\t' +
-                          word.attrib['wid'] + '\t' + '' + '\n')
-                    missing_lemmata.append(word.attrib['wid'])
+                        else:
+                            output.write(
+                                sentence.attrib['document_id'] + '\t' + sentence.attrib['subdoc'] + '\t' + sentence.attrib[
+                                    'id'] + '\t' + '' + '\t' +
+                                word.attrib['wid'] + '\t' + '' + '\n')
+                            missing_lemmata.append(word.attrib['wid'])
 
-                else:
-                    output.write(sentence.attrib['document_id'] + '\t' + '' + '\t' + sentence.attrib['id'] + '\t' +
-                          word.attrib['wid'] + '\t' + '' + '\n')
-
+                    else:
+                        if 'line' in word.attrib:
+                            output.write(sentence.attrib['document_id'] + '\t' + '' + '\t' + sentence.attrib['id'] + '\t' +
+                                         word.attrib['line'] + '\t' +
+                                         word.attrib['wid'] + '\t' + '' + '\n')
+                        else:
+                            output.write(sentence.attrib['document_id'] + '\t' + '' + '\t' + sentence.attrib[
+                                'id'] + '\t' + '' + '\t' +
+                                         word.attrib['wid'] + '\t' + '' + '\n')
 
 with open('missing_subdocs.txt', 'w+', encoding='UTF-8') as output:
     for missing in set(missing_subdocs):
